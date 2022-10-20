72ae02e4635cd383fa7cb4814c3e224303fe3b12
Demir Mesut ÖZTAŞ <48533725+DemirMesutOztas@users.noreply.github.com>
2022-10-20 14:23:34 +0300

    second

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,47 @@ public class Main
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
-        int w = in.nextInt();
+        System.out.print("Satır sayısı: ");
         int h = in.nextInt();
+        System.out.print("Sütun sayısı: ");
+        int w = in.nextInt();
+
         MineSweeper sample = new MineSweeper(w, h);
 
-        for (int i: sample.mayin)
+        for (int [] row: sample.mayin)
         {
-            System.out.println(i);
+            for(int i : row)
+            {
+                System.out.print(i+"\t");
+            }
+            System.out.println("");
+        }
+
+        System.out.println("-----------\n");
+
+
+       // System.out.println(sample.x);
+        //System.out.println(sample.height);
+       // System.out.println(sample.weight);
+
+        for(int i =0; i<sample.tahta.length;i++)
+        {
+            for(int j=0; j<sample.tahta[i].length;j++)
+            {
+                System.out.print(sample.tahta[i][j]);
+            }
+            System.out.println("");
+        }
+
+        System.out.println("-----------\n");
+
+        for(int i =0; i<sample.tahtaHarita.length;i++)
+        {
+            for(int j=0; j<sample.tahtaHarita[i].length;j++)
+            {
+                System.out.print(sample.tahtaHarita[i][j]);
+            }
+            System.out.println("");
         }
 
     }
--- src/MineSweeper.java
+++ src/MineSweeper.java
@@ -1,11 +1,13 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class MineSweeper
 {
     int weight, height;
     int x;
-    int [] mayin = new int[x];
+    int [][] mayin = new int[x][2];
     String [][] tahta = new String[weight][height];
+    String [][] tahtaHarita = new String[weight][height];
 
 
     MineSweeper(int w, int h)
@@ -13,39 +15,62 @@ public class MineSweeper
         this.weight = w;
         this.height = h;
         this.x = (w*h)/4;
-        this.tahta = new String[this.weight][this.height];
+        this.tahta = new String[this.height][this.weight];
+        this.tahtaHarita = new String[this.height][this.weight];
+        this.doldur();
+        this.mayinArea();
+
+    }
+
+    void doldur()
+    {
 
         for(int i = 0; i<this.tahta.length; i++)
         {
             for(int j = 0; j<this.tahta[i].length; j++)
             {
+
                 this.tahta[i][j]=" - ";
             }
 
         }
+        for(int i = 0; i<this.tahtaHarita.length; i++)
+        {
+            for(int j = 0; j<this.tahtaHarita[i].length; j++)
+            {
+
+                this.tahtaHarita[i][j]=" - ";
+            }
+
+        }
+    }
+     void mayinArea()
+    {
+
         Random r = new Random();
-        mayin = new int[this.x];
-        int ara;
-        while(x>0)
+        this.mayin = new int[this.x][2];
+        int ara, ara2;
+
+        int []tekDizi = new int[this.x];
+
+        for(int j = 0 ;j<this.x; j++)
         {
+            ara = r.nextInt(this.height);
+            ara2 = r.nextInt(this.weight);
 
+
+            this.mayin[j][0]=ara;
+            this.mayin[j][1]=ara2;
+
+
+            this.tahtaHarita[ara][ara2]= " * ";
         }
-        for(int k=0; k<this.x; k++)
-        {
-            if(k==0)
-            {
-
-                mayin[k]=ara;
-            }
-            else
-            {
-                ara=r.nextInt(x);
-                if()
-            }
-
-        }
-
 
 
     }
+
+
+
+
+
 }
